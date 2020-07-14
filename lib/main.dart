662abdf6b5c79870d531119a89a9818d04e57abe
jemmec <spogjem@gmail.com>
2020-07-15 00:09:05 +0930

    Added basic main screen

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Lifeforce',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Lifeforce'),
     );
   }
 }
@@ -28,11 +28,89 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var _darkGrey = Color.fromRGBO(68, 68, 68, 1.0);
+  var _midGrey = Color.fromRGBO(112, 112, 112, 1.0);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: <Widget>[],
+      body: SizedBox.expand(
+        child: Stack(
+          // mainAxisAlignment: MainAxisAlignment.end,
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    color: Colors.tealAccent,
+                    height: 56,
+                  ),
+                ),
+              ],
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                child: SizedBox(
+                  height: 45.0,
+                  width: 80.0,
+                  child: RaisedButton(
+                    color: Colors.black,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(28.0),
+                      topRight: Radius.circular(28.0),
+                    )),
+                    child: Icon(
+                      Icons.settings,
+                      size: 25.0,
+                      color: Colors.white,
+                    ),
+                    highlightElevation: 10.0,
+                    onPressed: () {
+                      showModalBottomSheet(
+                          enableDrag: true,
+                          backgroundColor: Colors.transparent,
+                          context: context,
+                          builder: (BuildContext context) {
+                            return Container(
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: <Widget>[
+                                  Container(
+                                    height: 18,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(28.0),
+                                        topRight: Radius.circular(28.0),
+                                      ),
+                                      color: Colors.lime,
+                                    ),
+                                  ),
+                                  Container(
+                                    height: (56 * 6).toDouble(),
+                                    color: Colors.lime,
+                                    child: Expanded(
+                                      child: Container(
+                                        margin: EdgeInsets.all(10.0),
+                                        color: Colors.orangeAccent,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            );
+                          });
+                    },
+                    onLongPress: () {},
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
